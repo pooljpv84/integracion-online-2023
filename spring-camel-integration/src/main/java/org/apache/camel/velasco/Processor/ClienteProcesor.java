050d57a286271c08b14da9854223dd5ef4035d87
paul <pooljpv84@hotmail.com>
2023-04-02 15:49:05 -0500

    fix cammel

--- spring-camel-integration/src/main/java/org/apache/camel/velasco/Processor/ClienteProcesor.java
+++ spring-camel-integration/src/main/java/org/apache/camel/velasco/Processor/ClienteProcesor.java
@@ -4,6 +4,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.camel.velasco.DTO.*;
 
 
 public class ClienteProcesor implements Processor{
--- spring-camel-integration/src/main/java/org/apache/camel/velasco/Processor/CreateClienteProcessor.java
+++ spring-camel-integration/src/main/java/org/apache/camel/velasco/Processor/CreateClienteProcessor.java
@@ -5,6 +5,7 @@ import org.apache.camel.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.modelmapper.ModelMapper;
+import org.apache.camel.velasco.DTO.*;
 
 
 public class CreateClienteProcessor implements Processor
